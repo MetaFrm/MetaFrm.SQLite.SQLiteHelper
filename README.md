c96ab749fa5090a3d3b74d8cfc0d25c309817f99
DSUN <dsun@hanmail.net>
2023-07-14 11:00:21 +0900

    2023-07-14_01

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # MetaFrm.SQLite.SQLiteHelper
+
+### .Net 6.0
+[![build 6.0](https://github.com/MetaFrm/MetaFrm.SQLite.SQLiteHelper/actions/workflows/build_6.0.yml/badge.svg)](https://github.com/MetaFrm/MetaFrm.SQLite.SQLiteHelper/actions/workflows/build_6.0.yml)
+[![NuGet version (MetaFrm.SQLite.SQLiteHelper.net6.0)](https://img.shields.io/nuget/v/MetaFrm.SQLite.SQLiteHelper.net6.0)](https://www.nuget.org/packages/MetaFrm.SQLite.SQLiteHelper.net6.0/)
+[![NuGet downloads (MetaFrm.SQLite.SQLiteHelper.net6.0)](https://img.shields.io/nuget/dt/MetaFrm.SQLite.SQLiteHelper.net6.0)](https://www.nuget.org/packages/MetaFrm.SQLite.SQLiteHelper.net6.0/)
+### .Net 7.0
+[![build 7.0](https://github.com/MetaFrm/MetaFrm.SQLite.SQLiteHelper/actions/workflows/build_7.0.yml/badge.svg)](https://github.com/MetaFrm/MetaFrm.SQLite.SQLiteHelper/actions/workflows/build_7.0.yml)
+[![NuGet version (MetaFrm.SQLite.SQLiteHelper.net7.0)](https://img.shields.io/nuget/v/MetaFrm.SQLite.SQLiteHelper.net7.0)](https://www.nuget.org/packages/MetaFrm.SQLite.SQLiteHelper.net7.0/)
+[![NuGet downloads (MetaFrm.SQLite.SQLiteHelper.net7.0)](https://img.shields.io/nuget/dt/MetaFrm.SQLite.SQLiteHelper.net7.0)](https://www.nuget.org/packages/MetaFrm.SQLite.SQLiteHelper.net7.0/)
---
+++ src/SQLiteHelper.cs
@@ -0,0 +1,100 @@
+﻿using SQLite;
+
+namespace MetaFrm.SQLite
+{
+    /// <summary>
+    /// SQLite를 처리하기 위한 클래스입니다.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SQLiteHelper<T> : ISQLiteHelper<T> where T : new()
+    {
+        private static readonly Dictionary<string, SQLiteAsyncConnection> connectionDictionary = new();
+
+
+        private static async Task ConnectionDB(string fullName)
+        {
+            if (!connectionDictionary.TryGetValue(fullName, out _))
+            {
+                string dbPath = Path.Combine(Factory.AppDataDirectory ?? "", $"{fullName.Replace(".", "")}.db3");
+
+                connectionDictionary.Add(fullName, new SQLiteAsyncConnection(dbPath));
+
+                // Create Table
+                if (connectionDictionary.TryGetValue(fullName, out SQLiteAsyncConnection? connection))
+                    await connection.CreateTableAsync<T>();
+            }
+        }
+
+        /// <summary>
+        /// Add
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public async Task<int> AddAsync(T value)
+        {
+            string fullName = $"{typeof(T).FullName}";
+
+            await ConnectionDB(fullName);
+
+            if (connectionDictionary.TryGetValue(fullName, out SQLiteAsyncConnection? connection))
+                return await connection.InsertAsync(value);
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public async Task<int> DeleteAsync(T value)
+        {
+            string fullName = $"{typeof(T).FullName}";
+
+            await ConnectionDB(fullName);
+
+            if (connectionDictionary.TryGetValue(fullName, out SQLiteAsyncConnection? connection))
+                return await connection.DeleteAsync(value);
+
+            return 0;
+        }
+
+        /// <summary>
+        /// GetList
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<T>> GetListAsync()
+        {
+            string fullName = $"{typeof(T).FullName}";
+
+            await ConnectionDB(fullName);
+
+            if (connectionDictionary.TryGetValue(fullName, out SQLiteAsyncConnection? connection))
+            {
+                var table = connection.Table<T>();
+
+                if (table != null)
+                    return await table.ToListAsync();
+            }
+
+            return new List<T>();
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public async Task<int> UpdateAsync(T value)
+        {
+            string fullName = $"{typeof(T).FullName}";
+
+            await ConnectionDB(fullName);
+
+            if (connectionDictionary.TryGetValue(fullName, out SQLiteAsyncConnection? connection))
+                return await connection.UpdateAsync(value);
+
+            return 0;
+        }
+    }
+}
